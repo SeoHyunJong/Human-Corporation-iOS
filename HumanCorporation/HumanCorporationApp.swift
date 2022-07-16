9d12271775093d4f659118ab6b1c2c38fc22dc77
SeoHyunJong <shj971219@gmail.com>
2022-07-16 15:31:17 +0900

    Initial Commit

---
+++ HumanCorporation/HumanCorporationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanCorporationApp.swift
+//  HumanCorporation
+//
+//  Created by 서현종 on 2022/07/16.
+//
+
+import SwiftUI
+
+@main
+struct HumanCorporationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
